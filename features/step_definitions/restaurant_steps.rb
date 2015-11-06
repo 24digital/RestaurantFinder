abbf5e23b0951125261f7f7c8d5815c090ca6c79
psadeghian <pasha1129@gmail.com>
2015-11-06 01:29:50 +0000

    Revert "filter by environment function: Gayathri"
    This reverts commit 41fd677767b0cc00beb050d257f38f3d28b4c569.

--- features/step_definitions/restaurant_steps.rb
+++ features/step_definitions/restaurant_steps.rb
@@ -10,42 +10,5 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
     all.index(e1).should <= all.index(e2) 
   end
 end
-When /I (un)?check the following environment checkboxes: (.*)/ do |uncheck_case, environment_list|
-    environment_list.split(/,\s*/).each do |environment| 
-      # \s 	Any whitespace character
-      # a* 	Zero or more of a
-      environment = "environments_#{environment}"
-      if uncheck_case 
-      # uncheck returns true or false depending on the regex above
-        uncheck(environment) 
-        #step %Q{I uncheck "#{environment}"}
-        # taken from web_steps.rb
-        # Capybara method
-      else
-        check(environment)
-        #step %Q{I check "#{environment}"}
-        # taken from web_steps.rb
-        # Capybara method
-      end
-    end
-  end
-  
-  Then /^I should (not )?see the following restaurants: (.*)$/ do |should_not_case, restaurant_list|
-    restaurant_list.split(/,\s*/).each do |restaurant|
-      step %Q{I should #{should_not_case}see "#{restaurant}"}
-    end
-  end
-  
-  
-  Then /^the following environment checkboxes should (not )?be checked: (.*)$/ do |should_not_case, environment_list|
-    environment_list.split(/,\s*/).each do |environment|
-      environment = "environments_#{environment}"
-      if should_not_case
-        has_unchecked_field?(environment).should == true
-        # Capybara method
-      else
-        has_checked_field?(environment).should == true
-        # Capybara method
-      end
-    end
-end
+
+
